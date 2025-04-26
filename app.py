924ec2bfe992e0f8cdbad747736db3c8731793d8
Venus-16 <6531501167@lamduan.mfu.ac.th>
2025-04-26 22:50:06 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 26 21:50:21 2025
+
+@author: User
+"""
+
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+
+# Load saved model and encoders
+model = pickle.load(open('loan_approval_model.pkl', 'rb'))
+status_encoder = pickle.load(open('employment_status_encoder.pkl', 'rb'))
+approval_encoder = pickle.load(open('approval_encoder.pkl', 'rb'))
+
+# Streamlit UI
+st.title("🏦 Loan Approval Prediction App")
+st.header("Project2_Group9")
+
+st.subheader("Enter Customer Details:")
+
+income = st.number_input('Annual Income ($)', min_value=20000, max_value=200000, step=1000)
+credit_score = st.slider('Credit Score', 300, 850, 600)
+loan_amount = st.number_input('Loan Amount ($)', min_value=1000, max_value=160000, step=1000)
+dti_ratio = st.slider('Debt-to-Income Ratio (%)', 0.0, 250.0, 30.0)
+employment_status = st.selectbox('Employment Status', ['employed', 'unemployed'])
+
+# Predict button
+if st.button('Predict Approval'):
+    # Prepare the input
+    status_encoded = status_encoder.transform([employment_status])[0]
+    
+    input_data = pd.DataFrame({
+        'Income': [income],
+        'Credit_Score': [credit_score],
+        'Loan_Amount': [loan_amount],
+        'DTI_Ratio': [dti_ratio],
+        'Employment_Status': [status_encoded]
+    })
+    
+    # Make prediction
+    prediction = model.predict(input_data)
+    prediction_label = approval_encoder.inverse_transform(prediction)[0]
+    
+    # Show result nicely
+    if prediction_label == 'Yes':
+        st.success("🎉 Loan Approved!")
+        st.balloons()
+    else:
+        st.error("❌ Loan Rejected!")
+
+
